effbd9e0478437b79aeda900f842153544f9b7b0
jurczakn <nickolas.jurczak@gmail.com>
2019-12-12 10:38:10 -0500

    added inital project

---
+++ src/test/java/com/revature/nmje/nmje/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.nmje.nmje;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
